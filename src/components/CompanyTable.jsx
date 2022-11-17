1861983916d1e7ec767ab094c788a97bc5864f2b
Cesar's PC <cesar261110@gmaill.com>
2022-11-16 19:27:51 -0800

    Pagination added! rows per page 10, rows in total is 100, something important is to .slice before .map

--- src/components/CompanyTable.jsx
+++ src/components/CompanyTable.jsx
@@ -1,17 +1,33 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import {useEffect, useState} from 'react';
-import {Box, Toolbar, Typography} from '@mui/material';
-import {getCompanyData} from '../company-data-generator';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+import { Box, Toolbar, Typography } from "@mui/material";
+import { getCompanyData } from "../company-data-generator";
+import { TablePagination } from "@mui/material";
 
 function CompanyTable() {
   let [rows, setRows] = useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   useEffect(() => {
     rows = getCompanyData();
@@ -19,16 +35,16 @@ function CompanyTable() {
   }, []);
 
   return (
-    <Box mt={20} sx={{width: '100%'}}>
-      <Paper sx={{width: '100%', mb: 2}}>
+    <Box mt={20} sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
         <Toolbar
           sx={{
-            pl: {sm: 2},
-            pr: {xs: 1, sm: 1},
+            pl: { sm: 2 },
+            pr: { xs: 1, sm: 1 },
           }}
         >
           <Typography
-            sx={{flex: '1 1 100%'}}
+            sx={{ flex: "1 1 100%" }}
             variant="h6"
             id="tableTitle"
             component="div"
@@ -37,22 +53,40 @@ function CompanyTable() {
           </Typography>
         </Toolbar>
         <TableContainer>
-          <Table
-            sx={{minWidth: 500}}
-          >
+          <Table sx={{ minWidth: 500 }}>
             <TableHead>
               <TableRow>
-                <TableCell width='20%' sx={{fontWeight: 'bold'}}>Company Name</TableCell>
-                <TableCell width='20%' align='right' sx={{fontWeight: 'bold'}}>What they do</TableCell>
-                <TableCell width='30%' align='right' sx={{fontWeight: 'bold'}}>Currency</TableCell>
-                <TableCell width='30%' align='right' sx={{fontWeight: 'bold'}}>Routing number</TableCell>
+                <TableCell width="20%" sx={{ fontWeight: "bold" }}>
+                  Company Name
+                </TableCell>
+                <TableCell
+                  width="20%"
+                  align="right"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  What they do
+                </TableCell>
+                <TableCell
+                  width="30%"
+                  align="right"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Currency
+                </TableCell>
+                <TableCell
+                  width="30%"
+                  align="right"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  Routing number
+                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
+              {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
                 <TableRow
                   key={row.id}
-                  sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
                   <TableCell component="th" scope="row">
                     {row.name}
@@ -66,6 +100,15 @@ function CompanyTable() {
           </Table>
         </TableContainer>
       </Paper>
+      <TablePagination
+        rowsPerPageOptions={[10, 20]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </Box>
   );
 }
